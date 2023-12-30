e61e4a3eed4a332455c59319e88b78059a034799
Siddiqui <mohd-faizan.siddiqui@capgemini.com>
2023-12-30 16:00:45 +0530

    Changes in Course component

--- src/component/Course.jsx
+++ src/component/Course.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React ,{useState} from 'react'
+import React, { useState } from 'react'
 import { toast } from 'react-toastify'
 import {
     Card,
@@ -40,7 +40,7 @@ function Course({ course, update }) {
             })
     }
 
-    //card display
+    //card display`
     return (
         <Card className='text-center'>
             <CardBody>
